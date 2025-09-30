056d9b02149c53fd71b65a426e6f05b66afdf10f
Sage <sagejacwilliams@yahoo.com>
2025-09-30 11:03:43 +0100

    added counter loop demo

---
+++ .venv/demo_counter_loops.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# Author: SWilliams
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
+"""
+count = 0
+while count < 10:
+    print(count)
+    count += 1
+"""
+
+for num in range(10):
+    print(num)
+
+
