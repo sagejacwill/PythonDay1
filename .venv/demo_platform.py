a9a3988cfaaee159ab274edbadb675055070febb
Sage <sagejacwilliams@yahoo.com>
2025-09-30 10:43:35 +0100

    added while loop for max attempts reached

--- .venv/demo_platform.py
+++ .venv/demo_platform.py
@@ -19,4 +19,20 @@ else:
 
 print("Home is", my_home)
 
+master_pin = "0123"
+pin = None
+attempts = 0
+
+while pin != master_pin and attempts < 3:
+    pin = input("Enter PIN: ")
+    if pin == master_pin:
+        print("Valid PIN")
+    else:
+        print("Invalid PIN")
+        attempts += 1
+else:
+    print("Maximum attempts reached")
+
+
+
 
