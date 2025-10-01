aa13b90e0a54d46fc2fa6d652b90174af8a48542
Sage <sagejacwilliams@yahoo.com>
2025-10-01 16:22:35 +0100

    CREATE: modern python package structure for a calculator app with app and test subpackages

--- calculator/main.py
+++ calculator/main.py
@@ -1,6 +1,37 @@
 #! /usr/bin/env python3
 # Author: SWilliams
-# Description: This script will demo
+# Description: This is a Calculator App
 """
-    Docstring:
+    Calculator App with Basic and Adv functions
 """
+import sys
+
+from calculator.app.basic import *
+from calculator.app.adv import *
+
+while True:
+    menu = """
+        Menu Options
+        ------------
+        1. Basic Calc Examples
+        2. Adv Calc Examples
+        q. Quit
+    """
+
+    print("********** CALC APP **********")
+    print(menu)
+    option = input("Enter option: 1-2, q=quit: ")
+
+    if option == "1":
+        print(f"9 * 8 * 7 = {add(9, 8, 7)}")
+        print(f"9 * 8 * 7 = {mul(9, 8, 7)}")
+        print(f"9 / 8 = {div(9, 8)}")
+    elif option == "2":
+        print(f"9 % 8  = {mod(9, 8)}")
+        print(f"9**8 = {power(9, 8)}")
+        print(f"\N{square root}98 = {sqrt(98)}")
+    elif option == "q":
+        break
+
+print("Done")
+sys.exit(0)
