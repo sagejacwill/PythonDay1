bbf43b571f9bede34ba0fac9d8d79798cba061dd
Sage <sagejacwilliams@yahoo.com>
2025-10-01 14:40:33 +0100

    CREATE: modern python package structure for a calculator app with app and test subpackages

---
+++ calculator/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# Author: SWilliams
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
---
+++ calculator/main.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+# Author: SWilliams
+# Description: This script will demo
+"""
+    Docstring:
+"""
