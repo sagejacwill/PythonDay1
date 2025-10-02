36d7d63f8f9484e7519365b1da5e93d74e871dcf
Sage <sagejacwilliams@yahoo.com>
2025-10-02 14:20:23 +0100

    CREATE: new flask app

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,13 +5,15 @@
     Docstring:
 """
 
-from flask import Flask, Response, request
+from flask import Flask, Response, request, render_template, url_for
 
 app = Flask(__name__)
 
+"""
 @app.route('/', methods=['GET'])
 def hello_from_flask():
     return 'Hello from Flask'
+"""
 
 @app.route('/sky', methods=['GET'])
 def hello_grads():
@@ -26,7 +28,36 @@ def post_text():
     data_sent = request.data.decode('utf-8')
     return Response(f"You posted{data_sent} to the Flask app", mimetype='text/plain')
 
+@app.route('/dynamic/<string:word>')
+def get_word(word):
+    return word
+
+@app.route('/square/<int:number>')
+def get_square(number):
+    return f"{number} squared is {number**2}"
+
+@app.route('/hello/<string:name>')
+def say_hello(name):
+    return f"Hello {name.title()}!"
+
+@app.route("/index/<name>/<int:iq>")
+def index(name, iq):
+    url = url_for("get_text")
+    return f"""
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+         <title>Sample - Flask routes</title>
+     </head>
+     <body>
+         <h1>Name Page</h1>
+         <p>Hello {name}!</p>
+         <p>Your IQ is {iq}!</p>
+         <hr>
+         <a href="{url}">Welcome</a>
+     </body>
+     </html>
+     """
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
