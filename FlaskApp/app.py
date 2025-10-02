38bd99ab6d413cf6407be3dd75fb545d0504532e
Sage <sagejacwilliams@yahoo.com>
2025-10-02 11:30:39 +0100

    CREATE: new flask app

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,7 +5,7 @@
     Docstring:
 """
 
-from flask import Flask
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,16 @@ def hello_from_flask():
 def hello_grads():
     return 'Hello Sky Grads'
 
+@app.route('/get/text', methods=['GET'])
+def get_text():
+    return Response("Hello from Flask, using an explicit Response Object")
+
+@app.route('/post/text', methods=['GET'])
+def post_text():
+    data_sent = request.data.decode('utf-8')
+    return Response(f"You posted{data_sent} to the Flask app", mimetype='text/plain')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
