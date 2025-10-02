d89c020fa84000ca3ad1a9f9840fea6bfeb07aaa
Sage <sagejacwilliams@yahoo.com>
2025-10-02 14:51:23 +0100

    CREATE: new flask app

--- FlaskApp2/app.py
+++ FlaskApp2/app.py
@@ -4,10 +4,9 @@
 """
     Docstring:
 """
-
 from flask import Flask
 
-app = Flask(__name__)
+from application import app
 
-from application import routes
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ FlaskApp2/application/routes.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# Author: SWilliams
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
+from flask import render_template
+from application import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
